57f44871e1522bb8569fc73013384dbdf4a2dc83
M <161216144+Aravind6281@users.noreply.github.com>
2024-05-28 20:17:38 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const API = axios.create({baseURL:"http://localhost:5500"})
-// const API = axios.create({baseURL:"https://youtubeclone-18.onrender.com"})
+// const API = axios.create({baseURL:"http://localhost:5500"})
+const API = axios.create({baseURL:"https://youtubecloneproject-1.onrender.com"})
 
 API.interceptors.request.use(req=>{
   if (localStorage.getItem('Profile')){
